fc7cec3df9ba2491fa1f86057b09792e06a0f59b
Xinhao Yuan <xinhaoyuan@gmail.com>
2018-10-21 22:42:40 -0400

    encountered strange performance problem ...

--- src/morpheus_ctl_calls.hrl
+++ src/morpheus_ctl_calls.hrl
@@ -21,3 +21,52 @@
 -define(cci_register_process(Node, Name, Proc), {register_process, Node, Name, Proc}).
 -define(cci_unregister(Node, Name), {unregister, Node, Name}).
 -define(cci_whereis(FromNode, Node, Name), {whereis, FromNode, Node, Name}).
+
+-define(cc_node_created(Ctl, Node),
+        call_ctl(Ctl, ?cci_node_created(Node))).
+
+-define(cc_get_opt(Ctl),
+        call_ctl(Ctl, ?cci_get_opt())).
+
+-define(cc_get_shtab(Ctl),
+        call_ctl(Ctl, ?cci_get_shtab())).
+
+-define(cc_initial_kick(Ctl),
+        call_ctl(Ctl, ?cci_initial_kick())).
+
+-define(cc_instrument_module(Ctl, M),
+        call_ctl(Ctl, ?cci_instrument_module(M))).
+
+-define(cc_undet(Ctl),
+        call_ctl(Ctl, ?cci_undet())).
+
+-define(cc_send_msg(Ctl, From, Where, To, Msg),
+        call_ctl(Ctl, ?cci_send_msg(From, Where, To, Msg))).
+-define(cc_undet_send_msg(Ctl, From, Where, To, Msg),
+        call_ctl(Ctl, {undet, ?cci_send_msg(From, Where, To, Msg)})).
+-define(cc_nodelay_send_msg(Ctl, From, Where, To, Msg),
+        call_ctl(Ctl, {nodelay, ?cci_send_msg(From, Where, To, Msg)})).
+
+-define(cc_send_signal(Ctl, From, Where, To, Reason),
+        call_ctl(Ctl, ?cci_send_signal(From, Where, To, Reason))).
+
+-define(cc_process_link(Ctl, From, To),
+        call_ctl(Ctl, ?cci_process_link(From, To))).
+
+-define(cc_process_unlink(Ctl, From, To),
+        call_ctl(Ctl, ?cci_process_unlink(From, To))).
+
+-define(cc_ets_all(Ctl),
+        call_ctl(Ctl, ?cci_ets_all())).
+
+-define(cc_get_clock(Ctl),
+        call_ctl(Ctl, ?cci_get_clock())).
+
+-define(cc_process_receive(Ctl, Proc, PatFun, Timeout),
+        call_ctl(Ctl, ?cci_process_receive(Proc, PatFun, Timeout))).
+
+-define(cc_process_on_exit(Ctl, Proc, Reason),
+        call_ctl(Ctl, ?cci_process_on_exit(Proc, Reason))).
+
+-define(cc_register_external_process(Ctl, Node, Name, Proc),
+        call_ctl(Ctl, ?cci_register_external_process(Node, Name, Proc))).
