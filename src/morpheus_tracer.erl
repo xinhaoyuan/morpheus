44c818afad581a05abb52d74a2d5863abb150d3c
Xinhao Yuan <xinhaoyuan@gmail.com>
2018-12-15 19:31:13 -0500

    tweaks

--- src/morpheus_tracer.erl
+++ src/morpheus_tracer.erl
@@ -208,10 +208,10 @@ merge_state_coverage(Tab, AccTab, SimpMap) ->
                           RState;
                       _ ->
                           SimpResult = simplify(RState, SimpMap),
-                          io:format(user,
-                                    "simplify: ~p~n"
-                                    "      to: ~p~n",
-                                    [RState, SimpResult]),
+                          %% io:format(user,
+                          %%           "simplify: ~p~n"
+                          %%           "      to: ~p~n",
+                          %%           [RState, SimpResult]),
                           SimpResult
                   end,
               case ets:insert_new(AccTab, {{state_coverage, SimpState}, 1}) of
