bc1f2f1d0b94e3be530772d1aa8577dced6bc98a
Xinhao Yuan <xinhaoyuan@gmail.com>
2018-12-16 11:35:35 -0500

    tweaks

--- src/morpheus_cmd.erl
+++ src/morpheus_cmd.erl
@@ -12,5 +12,16 @@ main(["show-acc-fanout", AccFilename]) ->
               #{D := N} = Fanout,
               io:format("~w: ~w~n", [D, N])
       end, lists:seq(0, MaxDepth));
+main(["show-states", AccFilename]) ->
+    {ok, AccTab} = ets:file2tab(AccFilename, [{verify,true}]),
+    StateCount =
+        ets:foldl(
+          fun ({{state_coverage, State}, Count}, Acc) ->
+                  io:format("~w: ~p~n", [Count, State]),
+                  Acc + 1
+          end, 0, AccTab),
+    [{state_coverage_count, TabCount}] = ets:lookup(AccTab, state_coverage_count),
+    TabCount = StateCount,
+    io:format("~w state listed.~n", [StateCount]);
 main(Args) ->
     io:format(standard_error, "Badarg: ~p~n", [Args]).
